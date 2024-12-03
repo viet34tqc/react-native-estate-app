0be2dbf01de5d4cd3066b196dedd0a61669f5b53
viet34tqc <viet34tqc@gmail.com>
2024-12-03 22:59:33 +0700

    Profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,177 @@
-import { useAuth } from '@clerk/clerk-expo';
-import React from 'react';
-import { Text, View } from 'react-native';
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/DefaultStyles';
+import { useAuth, useUser } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+import { Link } from 'expo-router';
+import React, { useState } from 'react';
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-const Profile = () => {
-  const { userId } = useAuth();
+const Page = () => {
+  const { signOut, isSignedIn } = useAuth();
+  const { user } = useUser();
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+  const [edit, setEdit] = useState(false);
+
+  // Update Clerk user data
+  // To use this feature, please go to Clerk dashboard => Email, phone, username => Personal information and turn it on
+  const onSaveUser = async () => {
+    try {
+      await user?.update({
+        firstName: firstName!,
+        lastName: lastName!,
+      });
+      setEdit(false);
+      setError('');
+    } catch (error) {
+      // TODO: if error, revert it back to the previous value
+      console.log(error);
+      setError(error instanceof Error ? error?.message : 'Failed to update');
+    } finally {
+      setEdit(false);
+    }
+  };
+
+  // Capture image from camera roll
+  // Upload to Clerk as avatar
+  const onCaptureImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 0.75,
+      base64: true,
+    });
+
+    if (!result.canceled) {
+      const base64 = `data:image/png;base64,${result.assets[0].base64}`;
+      user?.setProfileImage({
+        file: base64,
+      });
+    }
+  };
+
   return (
-    <View>
-      <Text>{userId && userId}</Text>
-    </View>
+    <SafeAreaView style={defaultStyles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Profile</Text>
+        <Ionicons name="notifications-outline" size={26} />
+      </View>
+
+      {user && (
+        <View style={styles.card}>
+          <TouchableOpacity onPress={onCaptureImage}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
+          </TouchableOpacity>
+          <View style={{ flexDirection: 'row', gap: 6 }}>
+            {!edit && (
+              <View style={styles.editRow}>
+                <Text style={{ fontFamily: 'mon-b', fontSize: 22 }}>
+                  {firstName} {lastName}
+                </Text>
+                <TouchableOpacity onPress={() => setEdit(true)}>
+                  <Ionicons
+                    name="create-outline"
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+            {edit && (
+              <View style={styles.editRow}>
+                <TextInput
+                  placeholder="First Name"
+                  value={firstName || ''}
+                  onChangeText={setFirstName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TextInput
+                  placeholder="Last Name"
+                  value={lastName || ''}
+                  onChangeText={setLastName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TouchableOpacity onPress={onSaveUser}>
+                  <Ionicons
+                    name="checkmark-outline"
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+          {error && <Text style={{ color: 'red' }}>{error}</Text>}
+          <Text>{email}</Text>
+          <Text>Since {user?.createdAt!.toLocaleDateString()}</Text>
+        </View>
+      )}
+
+      {isSignedIn && (
+        <Button title="Log Out" onPress={() => signOut()} color={Colors.dark} />
+      )}
+      {!isSignedIn && (
+        <Link href={'/(modals)/login'} asChild>
+          <Button title="Log In" color={Colors.dark} />
+        </Link>
+      )}
+    </SafeAreaView>
   );
 };
 
-export default Profile;
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 24,
+  },
+  header: {
+    fontFamily: 'mon-b',
+    fontSize: 24,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    alignItems: 'center',
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  editRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+});
+
+export default Page;
